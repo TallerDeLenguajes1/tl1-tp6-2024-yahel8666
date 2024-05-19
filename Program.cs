9efd03b21598f5651bfe66a374c19f87a113ada4
yahel8666 <yahelcamisay@gmail.com>
2024-05-19 04:11:40 -0300

    Terminado

--- Program.cs
+++ Program.cs
@@ -6,3 +6,23 @@ a=10;
 b=a;
 Console.WriteLine("valor de a:"+a);
 Console.WriteLine("valor de b:"+b); 
+
+//invertir numero 
+int n=0; 
+int invertido=0; 
+int ultimo; 
+Console.ReadLine("Ingrese un numero: \n"); 
+string s = Console.ReadLine(); 
+if (int.TryParse(s, out n)) //si la conversión a entero es exitosa...  
+{
+    while (n!=0)
+    {
+        ultimo = n%10;
+        invertido = invertido*10+ultimo; 
+        n = n/10; 
+    }
+}
+Console.WriteLine("El numero invertido es: "+invertido); 
+
+
+
